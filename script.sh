888902029fb2ad9073f65273650c7d72c58a1dc2
Vamsi Maradana <46924016+vmaradana1@users.noreply.github.com>
2023-04-13 14:57:21 +0100

    Create script.sh
    Added new file Script.sh

---
+++ script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "I am being run from a script"
+touch file1.txt file2.txt file3.txt
+echo "Created file1.txt, file2.txt & file3.txt"
+echo "Listing current directory contents"
+ls -al
